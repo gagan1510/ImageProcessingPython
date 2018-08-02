7ef87b7f88e1ea0edcf09f1efad48b73f6d6b053
Gagan Singh <gaganpreet.gs007@gmail.com>
2018-08-02 08:22:30 +0530

    Feature Matching Final

--- featureMatchingBruteForce.py
+++ featureMatchingBruteForce.py
@@ -2,8 +2,8 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
-img1 = cv2.imread('onePillow.jpg', 0)
-img2 = cv2.imread('multiplePillows.jpg', 0)
+img1 = cv2.imread('GF1.jpg', 0)
+img2 = cv2.imread('GF2.jpg', 0)
 
 # ORB is intended to be rotation invariant and deals well with noise, while being much faster than other feature detectors of the past ages.
 # this detects if a point on the image is a feature or not.
@@ -23,6 +23,6 @@ matches = bf.match(des1, des2)
 matches = sorted(matches, key = lambda x:x.distance)
 
 # show the matches in an image and specifying the number of matches from img1 and img2
-img3 = cv2.drawMatches(img1, kp1, img2, kp2, matches[:20], None, flags = 2)
+img3 = cv2.drawMatches(img1, kp1, img2, kp2, matches[:100], None, flags = 2)
 plt.imshow(img3)
 plt.show()
